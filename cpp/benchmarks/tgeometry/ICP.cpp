be29cf518ddd81834548c0455f281db25b73ef89
Yixing Lao <yixing.lao@gmail.com>
2021-03-03 22:19:45 -0800

    benchmark with openmp working

--- cpp/benchmarks/tgeometry/ICP.cpp
+++ cpp/benchmarks/tgeometry/ICP.cpp
@@ -33,7 +33,78 @@ namespace open3d {
 namespace t {
 namespace geometry {
 
-void ICPBenchmark(benchmark::State& state, const core::Device& device) {
+static void ComputePosePointToPlaneCPU(const float *src_pcd_ptr,
+                                       const float *tar_pcd_ptr,
+                                       const float *tar_norm_ptr,
+                                       const int64_t *corres_first,
+                                       const int64_t *corres_second,
+                                       const int n,
+                                       core::Tensor &pose,
+                                       const core::Dtype dtype,
+                                       const core::Device device) {
+    core::Tensor ATA =
+            core::Tensor::Zeros({6, 6}, core::Dtype::Float64, device);
+    core::Tensor ATA_1x21 =
+            core::Tensor::Zeros({1, 21}, core::Dtype::Float64, device);
+    core::Tensor ATB =
+            core::Tensor::Zeros({6, 1}, core::Dtype::Float64, device);
+
+    double *ata_ptr = static_cast<double *>(ATA.GetDataPtr());
+    double *ata_1x21 = static_cast<double *>(ATA_1x21.GetDataPtr());
+    double *atb_ptr = static_cast<double *>(ATB.GetDataPtr());
+
+#pragma omp parallel for reduction(+ : atb_ptr[:6], ata_1x21[:21])
+    for (int64_t workload_idx = 0; workload_idx < n; ++workload_idx) {
+        const int64_t &source_index = 3 * corres_first[workload_idx];
+        const int64_t &target_index = 3 * corres_second[workload_idx];
+
+        const float &sx = (src_pcd_ptr[source_index + 0]);
+        const float &sy = (src_pcd_ptr[source_index + 1]);
+        const float &sz = (src_pcd_ptr[source_index + 2]);
+        const float &tx = (tar_pcd_ptr[target_index + 0]);
+        const float &ty = (tar_pcd_ptr[target_index + 1]);
+        const float &tz = (tar_pcd_ptr[target_index + 2]);
+        const float &nx = (tar_norm_ptr[target_index + 0]);
+        const float &ny = (tar_norm_ptr[target_index + 1]);
+        const float &nz = (tar_norm_ptr[target_index + 2]);
+
+        float ai[] = {(nz * sy - ny * sz),
+                      (nx * sz - nz * sx),
+                      (ny * sx - nx * sy),
+                      nx,
+                      ny,
+                      nz};
+
+        for (int i = 0, j = 0; j < 6; j++) {
+            for (int k = 0; k <= j; k++) {
+                // ATA_ {1,21}, as ATA {6,6} is a symmetric matrix.
+                ata_1x21[i] += ai[j] * ai[k];
+                i++;
+            }
+            // ATB {6,1}.
+            atb_ptr[j] +=
+                    ai[j] * ((tx - sx) * nx + (ty - sy) * ny + (tz - sz) * nz);
+        }
+    }
+
+    // ATA_ {1,21} to ATA {6,6}.
+    for (int i = 0, j = 0; j < 6; j++) {
+        for (int k = 0; k <= j; k++) {
+            ata_ptr[j * 6 + k] = ata_1x21[i];
+            ata_ptr[k * 6 + j] = ata_1x21[i];
+            i++;
+        }
+    }
+
+    // // ATA(6,6) . Pose(6,1) = ATB(6,1)
+    // pose = ATA.Solve(ATB).Reshape({-1}).To(dtype);
+    // Solving_Pose_time_.Stop();
+    // utility::LogInfo("         Solving_Pose. Time: {}",
+    //                  Solving_Pose_time_.GetDuration());
+}
+
+void ICPBenchmark(benchmark::State &state, const core::Device &device) {
+    core::Dtype dtype = core::Dtype::Float32;
     core::Tensor source_points_contiguous = core::Tensor::Load(
             TEST_DATA_DIR "/icp_benchmark/source_points.npy");
     core::Tensor target_points_contiguous = core::Tensor::Load(
@@ -44,19 +115,25 @@ void ICPBenchmark(benchmark::State& state, const core::Device& device) {
             core::Tensor::Load(TEST_DATA_DIR "/icp_benchmark/corres_first.npy");
     core::Tensor corres_second_contiguous = core::Tensor::Load(
             TEST_DATA_DIR "/icp_benchmark/corres_second.npy");
-
-    int64_t num_points = 1000000;  // 1M
-    PointCloud pcd(device);
-    pcd.SetPoints(core::Tensor({num_points, 3}, core::Dtype::Float32, device));
-    pcd.SetPointColors(
-            core::Tensor({num_points, 3}, core::Dtype::Float32, device));
+    core::Tensor pose = core::Tensor::Empty({6}, dtype, device);
+    int n = 144132;
 
     // Warm up.
-    open3d::geometry::PointCloud legacy_pcd = pcd.ToLegacyPointCloud();
-    (void)legacy_pcd;
+    ComputePosePointToPlaneCPU(source_points_contiguous.GetDataPtr<float>(),
+                               target_points_contiguous.GetDataPtr<float>(),
+                               target_normals_contiguous.GetDataPtr<float>(),
+                               corres_first_contiguous.GetDataPtr<int64_t>(),
+                               corres_second_contiguous.GetDataPtr<int64_t>(),
+                               n, pose, dtype, device);
 
     for (auto _ : state) {
-        open3d::geometry::PointCloud legacy_pcd = pcd.ToLegacyPointCloud();
+        ComputePosePointToPlaneCPU(
+                source_points_contiguous.GetDataPtr<float>(),
+                target_points_contiguous.GetDataPtr<float>(),
+                target_normals_contiguous.GetDataPtr<float>(),
+                corres_first_contiguous.GetDataPtr<int64_t>(),
+                corres_second_contiguous.GetDataPtr<int64_t>(), n, pose, dtype,
+                device);
     }
 }
 
